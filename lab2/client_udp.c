f947585ead23ce8adec25d9f85aa9259c36cf944
poe <robakzdobywca@gmail.com>
2017-10-22 23:20:16 +0200

    doing stuff the lab way (Lukasz tak moze byc?)

--- lab2/client_udp.c
+++ lab2/client_udp.c
@@ -13,53 +13,43 @@
 #define BUFSIZE 1024
 #define PORT_NUMBER argv[2]
 #define HOST argv[1]
+#define MESSAGE argv[3]
 
 
 int main(int argc, char **argv) {
-    int sockfd, portno, n;
+    int sockfd, n;
     int serverlen;
     struct sockaddr_in serveraddr;
-    struct hostent *server;
-    char *hostname;
+    struct hostent *hp;
     char buf[BUFSIZE];
 
     //input error control
-    if (argc != 3) {
-       fprintf(stderr,"Wrong number of arguments\n Try: %s <hostname> <port>\n", argv[0]);
+    if (argc != 4) {
+       fprintf(stderr,"Wrong number of arguments\n Try: %s <hostname> <port> <msg>\n", argv[0]);
        exit(0);
     }
-    hostname = HOST;
-    portno = atoi(PORT_NUMBER);
 
     //create the socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);        //SOCK_DGRAM is dor UDP
     if (sockfd < 0) {
-        perror("ERROR opening socket");
+        perror("ERROR while creating socket");
         exit(1);
     }
 
-    //gethostbyname: get the server's DNS entry
-    server = gethostbyname(hostname);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
-        exit(0);
-    }
-
     //build the server's Internet address
-    bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-    serveraddr.sin_port = htons(portno);
+    hp = gethostbyname(HOST);
+    bcopy((char *)hp->h_addr, (char *)&serveraddr.sin_addr, hp->h_length);
+    if (hp == 0) {
+        perror("Unknown host");
+        exit(1);
+    }
+    serveraddr.sin_port = htons(atoi(PORT_NUMBER));
 
-    //get a message from the user
-    bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
 
     //send the message to the server
     serverlen = sizeof(serveraddr);
-    n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
+    n = sendto(sockfd, MESSAGE, strlen(MESSAGE), 0, &serveraddr, serverlen);
     if (n < 0) {
         perror("ERROR in sendto");
         exit(1);
--- lab2/server_udp.c
+++ lab2/server_udp.c
@@ -19,14 +19,10 @@
 
 int main(int argc, char **argv) {
     int sockfd;               /* socket */
-    int portno;               /* port to listen on */
     int clientlen;            /* byte size of client's address */
     struct sockaddr_in serveraddr; /* server's addr */
     struct sockaddr_in clientaddr; /* client addr */
-    struct hostent *hostp;    /* client host info */
     char buf[BUFSIZE];        /* message buf */
-    char *hostaddrp;          /* dotted decimal host addr string */
-    int optval;               /* flag value for setsockopt */
     int n;                    /* message byte size */
 
     //input error control
@@ -34,32 +30,18 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Wrong number of arguments\nTry: %s <port_number>\n", argv[0]);
         exit(1);
     }
-    portno = atoi(PORT_NUMBER);
 
     //create parent socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-      perror("Error while opening socket");
-      exit(1);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);        //SOCK_DGRAM is for UDP
+    if (sockfd == -1) {
+        perror("Error while opening socket");
+        exit(1);
     }
 
-    /* setsockopt: Handy debugging trick that lets
-    * us rerun the server immediately after we kill it;
-    * otherwise we have to wait about 20 secs.
-    * Eliminates "ERROR on binding: Address already in use" error.
-    */
-
-    optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
-	     (const void *)&optval , sizeof(int));
-
-
-         //build the server's Internet address
-    bzero((char *) &serveraddr, sizeof(serveraddr));
+    //build the server's Internet address
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons((unsigned short)portno);
-
+    serveraddr.sin_port = htons(atoi(PORT_NUMBER));
 
     if (bind(sockfd, (struct sockaddr *) &serveraddr,
     sizeof(serveraddr)) < 0) {
@@ -72,42 +54,22 @@ int main(int argc, char **argv) {
 
     do {
     //receive data from the client
-    bzero(buf, BUFSIZE);
     n = recvfrom(sockfd, buf, BUFSIZE, 0,
         (struct sockaddr *) &clientaddr, &clientlen);
     if (n < 0) {
-        perror("ERROR in recvfrom");
+        perror("ERROR while receiving data (recvfrom)");
         exit(1);
     }
 
-    /*
-    //gethostbyaddr: determine who sent the datagram
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL) {
-        perror("ERROR on gethostbyaddr");
-        exit(1);
-    }
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL) {
-        perror("ERROR on inet_ntoa\n");
-        exit(1);
-    }
-    printf("server received datagram from %s (%s)\n",
-        hostp->h_name, hostaddrp);
+    //what does the fox say
+    printf("Client said: %s\n", buf);
 
-    */
-    
-    printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
-
-
-
-    //sendto: echo the input back to the client
-    n = sendto(sockfd, buf, strlen(buf), 0,
+    //send message to the client
+    n = sendto(sockfd, "Got your message, bud.\n", 20, 0,
         (struct sockaddr *) &clientaddr, clientlen);
 
     if (n < 0) {
-        perror("ERROR in sendto");
+        perror("ERROR while sending message to the client");
         exit(1);
     }
 } while (ENDLESS_LOOP);
